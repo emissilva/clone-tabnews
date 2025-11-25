3e2d25d9412fb196b6b62decdd2e011a6f233da6
Emerson Silva <emerson.esilva@outlook.com>
2025-11-25 03:35:00 +0000

    adicao da frase para namorada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>ei, se você me ama, da uma risadinha!! 😋</h1>
+    return <h1>ei, enfermeira mais brilhante desse universo, eu amo vc!! 😋</h1>
 }
 
 export default Home;
