2a0a95726d6cf3194612e22f397db8e37f7b9c85
Emerson Silva <emerson.esilva@outlook.com>
2025-11-27 00:53:05 +0000

    formatação dos códigos usando prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo a desenvolver e a programar.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>ei, enfermeira mais brilhante desse universo, eu amo vc!! 😋</h1>
+  return <h1>ei, enfermeira mais brilhante desse universo, eu amo vc!! 😋</h1>;
 }
 
 export default Home;
